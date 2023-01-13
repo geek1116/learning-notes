c230eb876cb7b62c00f4874f6990c02d2ed733a2
geek1116\wudongsheng <1035214549@qq.com>
2023-01-13 21:49:38 +0800

    update 手写节流函数

--- 手写代码题/手写节流函数.js
+++ 手写代码题/手写节流函数.js
@@ -1,8 +1,11 @@
-function throttle(func, wait, options={}) {
-    let timer, previous = 0
+function throttle(func, wait, options={ leading: true, trailing: true }) {
+    let timer = null, previous = 0
 
     let throttled = function(...args) {
         const context = this, now = new Date().valueOf()
+
+        if(!previous && !options.leading) previous = now
+
         const remainTime = wait - (now - previous)  // 剩余时间
         
         if(remainTime <=0 || remainTime > wait) {   // 剩余时间小于0 或者手动修改了系统时间
@@ -14,13 +17,19 @@ function throttle(func, wait, options={}) {
             previous = now
             return func.apply(context, args)
 
-        } else if(!timer) {
+        } else if(!timer && options.trailing) {
             timer = setTimeout(function() {
-                previous = new Date().valueOf()
+                previous = options.leading === false ? 0 : new Date().valueOf()
                 timer = null
                 func.apply(context, args)
             }, remainTime)
         }
+    }
+
+    throttled.cancel = function() {
+        previous = 0
+        clearTimeout(timer)
+        timer = null
     }
 
     return throttled
